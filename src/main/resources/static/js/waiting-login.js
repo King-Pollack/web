143bc49dfa5b4d3f3112a1eca12c98d001f5c0a6
HYKim <ddh963963@naver.com>
2023-12-24 00:27:12 +0900

    [#45] refactor : static -> path 동적으로 수정, js 분리

---
+++ src/main/resources/static/js/waiting-login.js
@@ -0,0 +1,8 @@
+var eventSource = new EventSource('/waiting/stream/total-team');
+eventSource.onmessage = function(event) {
+    var dataContainer = document.getElementById("data-container");
+    dataContainer.innerHTML = "";
+    var newData = document.createElement("p");
+    newData.innerText = '현재 대기 팀 : ' + event.data + ' 팀'
+    dataContainer.appendChild(newData);
+};
